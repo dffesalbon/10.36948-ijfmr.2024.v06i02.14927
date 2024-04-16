7a9b509deebc25ad88bd7c2f528c03f8b0228274
Daniel Fesalbon <86942695+dffesalbon@users.noreply.github.com>
2024-04-16 17:24:16 +0800

    delete: code

--- code/scrape/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#exec > ./console.txt 2>&1
-echo "Scraping data.."
-py ./scrape.py 100
-echo "Consolidating.."
-py ./consolidate.py
-echo "Filtering english detected chats.."
-py ./filter.py
